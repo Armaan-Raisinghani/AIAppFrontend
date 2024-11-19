e27824ad558e232c484919acff04b6aa9a4ebe0c
Shreya-Khanna <shreyakhanna191205@gmail.com>
2024-11-20 04:56:43 +0530

    instrux

--- app/description.tsx
+++ app/description.tsx
@@ -4,18 +4,70 @@ import { View, Text, TouchableOpacity } from "react-native";
 // Entry Screen Component
 const descriptionScreen = () => {
   return (
-    <View className="flex-1 p-6  bg-white">
-      {/* Main Container */}
-      <View className="w-full items-center mt-8 bg-teal-800 p-4 ">
-        <Text className="text-2xl font-bold text-white">Instruction</Text>
+    <<<<<<< HEAD
+import React from "react";
+import { View, Text, Touchable, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function App() {
+  return (
+    <SafeAreaView className="flex-1 bg-white-200 items-center justify-center">
+      {/* Outer Container */}
+      <View className="bg-white w-11/12 h-5/6 rounded-lg shadow-lg">
+        {/* Header Section */}
+        <View className="bg-teal-700 w-10/12 mx-auto mt-8 p-4">
+          <Text className="text-white text-center text-lg font-bold">
+            Instructions
+          </Text>
+        </View>
+        {/* Content Section */}
+        <View className="bg-teal-700 w-5/6 mx-auto my-4 p-4 h-[60%]"></View>
+        <TouchableOpacity>
+        <View className="bg-teal-700 w-auto mx-auto my-8 p-4 rounded-full">
+          <Text className="text-white text-center text-lg font-bold">
+            Enter
+          </Text>
+        </View>
+        </TouchableOpacity>
       </View>
-      <View className="w-full items-center my-8 bg-teal-800 p-4">
-        <Text className="text-lg text-white">Instruction</Text>
+    </SafeAreaView>
+  );
+}
+=======
+import { router, useNavigation } from "expo-router";
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+// Entry Screen Component
+const descriptionScreen = () => {
+  return (
+    <SafeAreaView className="flex-1 bg-white-200 items-center justify-center">
+      {/* Outer Container */}
+      <View className="bg-white w-11/12 h-5/6 rounded-lg shadow-lg">
+        {/* Header Section */}
+        <View className="bg-teal-700 w-10/12 mx-auto mt-8 p-4">
+          <Text className="text-white text-center text-lg font-bold">
+            Instructions
+          </Text>
+        </View>
+        {/* Content Section */}
+        <View className="bg-teal-700 w-5/6 mx-auto my-4 p-4 h-[60%]"></View>
+        <TouchableOpacity>
+        <View className="bg-teal-700 w-auto mx-auto my-8 p-4 rounded-full">
+          <Text className="text-white text-center text-lg font-bold">
+            Enter
+          </Text>
+        </View>
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity>
-        <Text className="text-lg text-white">Start</Text>
-      </TouchableOpacity>
-    </View>
+    </SafeAreaView>
+  );
+};
+
+// Export NativeWind Stylesheet
+export default descriptionScreen;
+>>>>>>> 46f9028344eb9273ffaaf99f9602eddc18363d7a
+
+
   );
 };
 
