eed03b75ab7253c5e22373d09939da343ef554c1
Armaan <raisinghani.armaan1@gmail.com>
2024-11-21 04:11:12 +0530

    index redesign

--- app/index.tsx
+++ app/index.tsx
@@ -6,8 +6,6 @@ const WelcomeScreen = () => {
     <View className="flex-1 bg-blue-400 justify-center items-center">
       {/* Main Card */}
       <View className="bg-white w-3/4 max-w-lg p-6 h-96 rounded-lg shadow-md">
-      
-
         {/* Title */}
         <Text className="text-xl font-bold text-center">Welcome</Text>
 
@@ -25,13 +23,23 @@ const WelcomeScreen = () => {
         </Text>
 
         {/* Enter Button */}
-        <TouchableOpacity className="bg-blue-800 py-2 px-4 mt-6 rounded-full">
+        <TouchableOpacity className="bg-blue-800 py-2 px-4 mt-6 rounded-full absolute bottom-10 self-center w-1/2">
           <Text className="text-center text-white font-semibold">Enter</Text>
         </TouchableOpacity>
       </View>
+      {/* Plaksha Logo */}
+      <Image
+        className=""
+        source={require("../assets/images/logo-white.png")}
+        style={{
+          width: 250,
+          height: 125,
+        }}
+        className="absolute bottom-20"
+      />
 
       {/* Bottom Buttons */}
-      <View className="absolute bottom-6 flex-row space-x-4">
+      <View className="absolute bottom-10 flex-row gap-4 ">
         {/* HTI Lab Button */}
         <TouchableOpacity className="bg-blue-500 py-2 px-4 rounded-md shadow-md">
           <Text className="text-white font-semibold">HTI LAB</Text>
@@ -42,16 +50,6 @@ const WelcomeScreen = () => {
           <Text className="text-white font-semibold">CTLC LAB</Text>
         </TouchableOpacity>
       </View>
-
-      {/* Plaksha Logo */}
-      <Image
-      source={require("../assets/images/logo-white.png")}
-          style={{
-            
-            width: 250,
-            height: 125,
-          }}
-        /> 
     </View>
   );
 };
